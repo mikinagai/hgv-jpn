0373de3479f3204322d1ecdde71062524f167a5c
Miki Nagai <mikinagai616@gmail.com>
2016-03-08 10:09:32 -0500

    New york index

--- new-york/index.php
+++ new-york/index.php
@@ -1,9 +1,9 @@
-<?php include("../../server-side/library.php");?>
+<?php include("../server-side/library.php"); ?>
 <!doctype html>
 <html>
 <head>
-  <meta name="description" content="The spectacular allure of the Las Vegas Strip delivers a one-of-a-kind vacation experience. The relaxing setting of Hilton Grand Vacations Club provides a serene oasis within easy access to the best of Las Vegas." />
-  <title>Hilton Grand Vacations Resort on Paradise in Las Vegas, Nevada</title>
+  <meta name="description" content="View the list of New York resorts that Hilton Grand Vacations has to offer." />
+  <title>New York Resorts by Hilton Grand Vacations</title>
   <?php addStyles(); ?>
 </head>
 <body>
@@ -13,275 +13,58 @@
     <?php addHeader(); ?>
     
     <div id="subbanner">
-
-      <div class="container-max">
-        <div id="resorts-options" class="hidden-xs">
-          <ul>
-            <li><a href="#" id="photos-link" class="active">写真</a></li>
-            <li><a href="#" id="floorplans-link">間取り</a></li>
-            <li><a href="#" id="points-link">必要ポイント数</a></li>
-            <li><a href="#" id="map-link">地図</a></li>
-          </ul>
-        </div>
-      </div>
     
-       <div id="homeslideshow"> 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner">
-              <div class="item active" id="slide1" style="background-image: url('img/banner01-s.jpg');"></div>
-              <div class="item" id="slide2" style="background-image: url('img/banner02-s.jpg');"></div>
-              <div class="item" id="slide3" style="background-image: url('img/banner03-s.jpg');"></div>
-              <div class="item" id="slide4" style="background-image: url('img/banner04-s.jpg');"></div>
-              <div class="item" id="slide5" style="background-image: url('img/banner05-s.jpg');"></div>
-          </div>
-
-          <!-- Left and right controls -->
-          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-
-        </div>
-      </div><!-- destinationslideshow -->
-      
-<!--floorplan start -->
-        
-
-      <div id="floorplan-container">
-      
-        <div class="row">
-          <div class="col-sm-3 text-center">
-            <div class="floorplan">
-              <p class="small"><img src="floorplans/studio-t.jpg" alt="Two-Bedroom Floor Plan at Grand Waikikian Resort in Honolulu, Hawaii" /></p>
-              <p class="large"><img src="floorplans/studio.jpg" alt="Two-Bedroom Floor Plan at Grand Waikikian Resort in Honolulu, Hawaii" /></p>
-              <p>スタジオ</p>
-            </div>
-          </div>
-          <div class="col-sm-3 text-center">
-            <div class="floorplan">
-              <p class="small"><img src="floorplans/one-t.jpg" alt="Two-Bedroom Floor Plan at Grand Waikikian Resort in Honolulu, Hawaii" /></p>
-              <p class="large"><img src="floorplans/one.jpg" alt="Two-Bedroom Floor Plan at Grand Waikikian Resort in Honolulu, Hawaii" /></p>
-              <p>1ベッドルーム</p>
-            </div>
-          </div>
-          <div class="col-sm-3 text-center">
-            <div class="floorplan">
-              <p class="small"><img src="floorplans/two-t.jpg" alt="Two-Bedroom Floor Plan at Grand Waikikian Resort in Honolulu, Hawaii" /></p>
-              <p class="large"><img src="floorplans/two.jpg" alt="Two-Bedroom Floor Plan at Grand Waikikian Resort in Honolulu, Hawaii" /></p>
-              <p>2ベッドルーム</p>
-            </div>
-          </div>
-        </div>
-        
-      </div> <!-- floorplan-container -->
-        
-       <!-- floorplan-container -->
-      
-      <div id="points-container">
-        <div class="row">
-        
-          <div class="col-sm-6">
-        
-            <div class="table-responsive">
-              <table class="table table-hover silver">
-                <thead>
-                  <tr>
-                    <th colspan="4"><img src="../../img/silver.gif" alt="" class="level" /> シルバー: 1 &ndash; 4週、49週、50週</th>
-                  </tr>
-                  <tr>
-                    <th>部屋タイプ</th>
-                    <th>月 &ndash; 木</th>
-                    <th>金 &ndash; 日</th>
-                    <th>7泊</th>
-                   </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>スタジオ</td>
-                    <td>110</td>
-                    <td>220</td>
-                    <td>1,100</td>
-                  </tr>
-                  <tr>
-                    <td>1ベッドルーム</td>
-                    <td>240</td>
-                    <td>480</td>
-                    <td>2,400</td>
-                  </tr>
-                  <tr>
-                    <td>2ベッドルーム</td>
-                    <td>350</td>
-                    <td>700</td>
-                    <td>3,500</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            
-          </div>
-        
-          <div class="col-sm-6">
-          
-            <div class="table-responsive">
-              <table class="table table-hover gold">
-                <thead>
-                  <tr>
-                    <th colspan="4"><img src="../../img/gold.gif" alt="" class="level" /> ゴールド: 5 &ndash; 8週、21 &ndash; 36週、44 &ndash; 48週</th>
-                  </tr>
-                  <tr>
-                    <th>部屋タイプ</th>
-                    <th>月 &ndash; 木</th>
-                    <th>金 &ndash; 日</th>
-                    <th>7泊</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>スタジオ</td>
-                    <td>160</td>
-                    <td>320</td>
-                    <td>1,600</td>
-                  </tr>
-                  <tr>
-                    <td>1ベッドルーム</td>
-                    <td>340</td>
-                    <td>680</td>
-                    <td>3,400</td>
-                  </tr>
-                  <tr>
-                    <td>2ベッドルーム</td>
-                    <td>500</td>
-                    <td>1,000</td>
-                    <td>5,000</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            
-          </div>
-            
-            
-          <div class="col-sm-6">
-          
-            <div class="table-responsive">
-              <table class="table table-hover platinum">
-                <thead>
-                  <tr>
-                    <th colspan="4"><img src="../../img/platinum.gif" alt="" class="level" /> プラチナ: 9 &ndash; 20週、37 &ndash; 43週、51週、52週</th>
-                  </tr>
-                  <tr>
-                    <th>部屋タイプ</th>
-                    <th>月 &ndash; 木</th>
-                    <th>金 &ndash; 日</th>
-                    <th>7泊</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>スタジオ</td>
-                    <td>220</td>
-                    <td>440</td>
-                    <td>2,200</td>
-                  </tr>
-                  <tr>
-                    <td>1ベッドルーム</td>
-                    <td>480</td>
-                    <td>960</td>
-                    <td>4,800</td>
-                  </tr>
-                  <tr>
-                    <td>2ベッドルーム</td>
-                    <td>700</td>
-                    <td>1,400</td>
-                    <td>7,000</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            
-          </div>
-            
-
-          
-        </div> <!-- row -->
+      <div id="destinationslideshow">
+        <img src="img/banner01.jpg" alt="Enjoy A Vacation in New York with Hilton Grand Vacations" />
+        <img src="img/banner02.jpg" alt="Enjoy A Family Vacation in New York with Hilton Grand Vacations" />
       </div>
       
-      <div id="map-container">
-        <div id="map"></div>
-      </div>
-    
-      
-      <div class="banner-caption resort-title">
-        <h1>ヒルトン・グランド・バケーションズ・クラブ・オン・パラダイス</h1>
+      <div class="banner-caption">
+        <h1>New York</h1>
       </div>
       
-    </div> <!-- subbanner -->
-    
-
-    <div id="subtitle">
-      <h2>アメリカ、ネバダ州ラスベガス</h2>
     </div>
     
-
-
-    <div id="subcontent" class="content">
+    <div id="subtitle">
+      <h2>Our Resorts</h2>
+    </div>
+    
+    <div class="content">
     
       <div class="row">
-      
-        <div class="col-sm-8 main-column">
-
-          <h3>概要</h3>
-          <address>455 Karen Ave, Las Vegas, NV 89109</address>
-          <p>目を見張るほど豪華できらびやかなラスベガス・ストリップ街と、壮大で美しい自然に恵まれたラスベガスのバケーションは格別です。ヒルトン・グランド・バケーションズ・クラブは、中心街の近くにありながら、静かにゆったり寛げる環境をお届けします。グランドキャニオン、 
-ミード湖、フーバー・ダムなど数多くの名所に加え、年間を通じてゴルフ、テニス、ボートなどをお楽しみいただけます。また、ラスベガス・コンベンション・センターに隣接した理想的な場所にあり、充実したレストランやホテルの施設に加え、他のアトラクションも目の前です。</p>
-          
-          <br />
-          
-          <h3>宿泊施設</h3>
-          <ul>
-            <li>スタジオ（調理設備なし）、1～2ベッドルーム・スイート</li>
-            <li>各スイートにフルキッチン、洗濯機、乾燥機完備</li>
-            <li>各スイートにジャグジー付きマスターバスルーム完備</li>
-            <li>高速ワイヤレス・インターネットアクセス完備（無料）</li>
-            <li>全室禁煙</li>
-          </ul>
-          
-          <br />
-          
-          <h3>リゾート設備</h3>
-          <ul>
-            <li>温水プールとジャグジー</li>
-            <li>パラダイスガーデン・ギフトショップとデリ</li>
-            <li>オーナーズ・ラウンジ</li>
-            <li>フィットネス施設</li>
-            <li>ヒルトン・グランド・バケーションズが管理</li>
-            <li>13年間の実績</li>
-            <li>身障者向け施設あり</li>
-            <li>RCIゴールドクラウン・リゾート</li>
-          </ul>
-          
-          <br />
-          
-          
-          
-          <br />
-            
+        <div class="col-md-12 text-center">
+          <div id="placeholder"></div>
         </div>
-        <div class="col-sm-4 aside">
-          <h3>追加情報</h3>
-          <p>チェックイン: 4 p.m.<br />チェックアウト: 10 a.m.<br />電話番号: 702-946-9200</p>
-          <p><a href="../../contact/" class="btn btn-secondary">リクエストフォーム</a></p>
-
-
-          
+      </div>
+    
+      <div id="resort-collection">
+      
+        <div class="row resort">
+          <div class="col-sm-4 text-center">
+            <img src="img/hiltonclub.jpg" alt="The Hilton Club &ndash; New York" />
+          </div>
+          <div class="col-sm-8">
+            <h3>The Hilton Club &ndash; New York</h3>
+            <p class="location">New York, New York, USA</p>
+            <p>This unique retreat in the heart of Manhattan serves as a spectacular getaway, delivering signature Hilton service and accommodations. Owners enjoy an array of amenities tailored to their distinctive expectations, including extensive dining and entertainment options.</p>
+            <p><a href="hilton-club/" class="btn btn-primary">Learn More &raquo;</a></p>
+          </div>
         </div>
         
+        <div class="row resort">
+          <div class="col-sm-4 text-center">
+            <img src="img/west57.jpg" alt="West 57th Street by Hilton Club" />
+          </div>
+          <div class="col-sm-8">
+            <h3>West 57th Street by Hilton Club</h3>
+            <p class="location">New York, New York, USA</p>
+            <p>Located in the heart of midtown Manhattan's prestigious Plaza District, West 57 th Street by Hilton Club is close to the city's most celebrated destinations, including Lincoln Center, Central Park, Carnegie Hall and Fifth Avenue's world-famous shopping.</p>
+            <p><a href="west-57th-street/" class="btn btn-primary">Learn More &raquo;</a></p>
+          </div>
+        </div>
+      
       </div>
+      
     </div>
     
     <?php addFooter(); ?>
@@ -289,14 +72,7 @@
   </div>
 
   <?php addScripts(); ?>
-  <script type="text/javascript" src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>
-  <script type="text/javascript" src="../../js/swipe.js"></script>
-  <script type="text/javascript" src="../../js/resorts.js"></script>
-  <script type="text/javascript">
-    $(document).ready(function(e){ 
-	  getMap(36.1399463, -115.1522803, 14);
-    });
-  </script>
+  <script type="text/javascript" src="../js/destinations.js"></script>
 
 </body>
 </html>
